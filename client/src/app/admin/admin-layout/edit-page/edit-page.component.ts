b6d973dd07f1e7cae5ecf9ce862bbacc8b7c9725
nktnlg <2eleven92@gmail.com>
2023-01-11 00:36:12 -0300

    parent folder selection 1part

--- client/src/app/admin/admin-layout/edit-page/edit-page.component.ts
+++ client/src/app/admin/admin-layout/edit-page/edit-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Subscription, switchMap } from 'rxjs';
-import { Note } from 'src/app/shared/interfaces';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable, Subscription, switchMap } from 'rxjs';
+import { FoldersService } from 'src/app/shared/folders.service';
+import { Note, Folder } from 'src/app/shared/interfaces';
 import { NotesService } from 'src/app/shared/notes.service';
 import { AlertService } from '../../admin-shared/services/alert.service';
 
@@ -15,23 +16,29 @@ import { AlertService } from '../../admin-shared/services/alert.service';
 export class EditPageComponent implements OnInit, OnDestroy{
 
   form!: FormGroup;
-  note!: Note;
+  note: Note| null = null;
   load = false;
   updateSub: Subscription| null = null;
+  folders$: Observable<Folder[]> | null = null;
 
   constructor(
     private route: ActivatedRoute, 
     private notesService: NotesService,
-    private alertService: AlertService){}
+    private foldersService: FoldersService,
+    private alertService: AlertService,
+    private router: Router){}
 
   submit(){
     if(this.form.invalid)return;
-    this.load = true;
+    if(!this.note)return;
 
+    this.load = true;
     this.updateSub = this.notesService.updateNote({
       ...this.note,
       text: this.form.value.text,
       title: this.form.value.title
+      //parentFolderId: this.form.value.folder.id,
+      //parentFolderName: this.form.value.folder.title,
     }).subscribe(()=>{this.load = false; this.alertService.success('Note edited')})
   };
 
@@ -40,13 +47,22 @@ export class EditPageComponent implements OnInit, OnDestroy{
       switchMap((params: Params)=>{
         return this.notesService.getNoteById(params['id'])
       })
-    ).subscribe((note: Note)=>{
+    ).subscribe(
+      (note: Note)=>{
         this.note = note;
         this.form = new FormGroup({
           title: new FormControl(note.title, Validators.required),
           text: new FormControl(note.text, Validators.required)
-    })
+    })}, 
+      (error)=>{
+        this.router.navigate(['/admin', 'dashboard']);
+        window.scroll({ 
+          top: 0, 
+          left: 0, 
+          behavior: 'smooth'})
       })
+
+      this.folders$ = this.foldersService.getFolders()
   }
 
   ngOnDestroy(): void {
